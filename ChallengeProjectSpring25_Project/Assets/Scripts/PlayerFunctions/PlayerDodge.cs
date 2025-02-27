09e3d9fce83b2a821d0696e4abb683a27e4016f2
bena3721 <161668366+bena3721@users.noreply.github.com>
2025-02-26 21:56:42 -0600

    Continued dodge script
    dodging no longer throws errors

--- ChallengeProjectSpring25_Project/Assets/Scripts/PlayerFunctions/PlayerDodge.cs
+++ ChallengeProjectSpring25_Project/Assets/Scripts/PlayerFunctions/PlayerDodge.cs
@@ -1,33 +1,36 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XR;
 using UnityEngine.Windows;
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerDodge : MonoBehaviour
 {
     private CharacterController controller;
-    private PlayerInput input;
     private Vector2 move;
     private PlayerMovement playerMovement;
+    [SerializeField] private PlayerInput input;
+    
 
-    [SerializeField] private float dodgeSpeed = 10f;
-
-
+    [SerializeField] private float dodgeSpeed = 25f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        input = GetComponent<PlayerInput>();
         playerMovement = GetComponent<PlayerMovement>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (input.actions["Dodge"].triggered)
+        
+        if (input.actions["Sprint"].triggered)
         {
+            Debug.Log("Dodging");
             playerMovement.enabled = false;
-
+            
             move = input.actions["Move"].ReadValue<Vector2>();
             Vector3 moveDirection = Vector3.zero;
             moveDirection = (transform.right * move.x + transform.forward * move.y).normalized;
