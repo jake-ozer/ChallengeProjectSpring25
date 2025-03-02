6f8202aa29a07a6a992cf46e514de489ae32d6a7
lennart3211 <82011287+lennart3211@users.noreply.github.com>
2025-03-02 14:25:07 -0600

    Improved fire spawning
    Fire now spawns on the floor layer

--- ChallengeProjectSpring25_Project/Assets/Scripts/GameFunctions/FireManager.cs
+++ ChallengeProjectSpring25_Project/Assets/Scripts/GameFunctions/FireManager.cs
@@ -1,15 +1,57 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class FireManager : MonoBehaviour
 {
     public GameObject[] firePrefabs;
     public LayerMask obstacleLayers;
-    private float groundLevel = -1.92f;
+    public LayerMask groundLayer;
+
+    // min/max boudns of where the fire will spawn
+    private Vector3 minBounds, maxBounds;
+
+    private Perlin perlin;
+    
+    // neccessary to avoid passing negative coordinates to the perlin function
+    private float perlinOffset = 500f;
+    
+    // these should correspond to the size of the fire assets
+    private float xStep = 1.5f, yStep = 1.5f;
 
     public void Start()
     {
+        perlin = new Perlin((int)Random.value);
+        findMinMax();
+        Debug.Log(minBounds);
+        Debug.Log(maxBounds);
         SpawnFire();
+    }
+
+    private void findMinMax()
+    {
+        minBounds = new Vector3(float.MaxValue, 0, float.MaxValue);
+        maxBounds = new Vector3(float.MinValue, 0, float.MinValue);
+
+        GameObject[] allObjects = GameObject.FindObjectsByType<GameObject>(FindObjectsInactive.Include, FindObjectsSortMode.InstanceID);
+        foreach (GameObject obj in allObjects)
+        {
+            if ((groundLayer.value & (1 << obj.layer)) != 0)
+            {
+                Renderer renderer = obj.GetComponent<Renderer>();
+                if (renderer)
+                {
+                    Bounds bounds = renderer.bounds;
+
+                    if (bounds.min.x < minBounds.x) minBounds.x = bounds.min.x;
+                    if (bounds.min.z < minBounds.z) minBounds.z = bounds.min.z;
+                    if (bounds.max.x > maxBounds.x) maxBounds.x = bounds.max.x;
+                    if (bounds.max.z > maxBounds.z) maxBounds.z = bounds.max.z;
+                }
+                
+                
+            }
+        }
     }
 
     public void SpawnFire()
@@ -17,11 +59,11 @@ public class FireManager : MonoBehaviour
         // TODO: Figure out spawning position
         // TODO: Account for things on the map
      
-        for (uint x = 0; x < 50; ++x)
+        for (float x = minBounds.x; x < maxBounds.x; x+=xStep)
         {
-            for (uint y = 0; y < 50; ++y)
+            for (float y = minBounds.z; y < maxBounds.z; y+=yStep)
             {
-                float noise = Mathf.PerlinNoise((float)x/5f, (float)y/5f);
+                float noise = perlin.perlin2d(x+perlinOffset, y+perlinOffset, 0.1f, 1);
                 int level = -1;
                 if (noise < 1 && noise > 0.9)
                 {
@@ -38,7 +80,18 @@ public class FireManager : MonoBehaviour
 
                 if (level > -1)
                 {
-                    Instantiate(firePrefabs[level], new Vector3(x, groundLevel, y), Quaternion.identity);
+                    Vector3 rayStart = new Vector3(x, 100, y);
+                    if(Physics.Raycast(rayStart, Vector3.down, out RaycastHit hit, 
+                           Mathf.Infinity, groundLayer))
+                    {
+                        float slopeAngle = Vector3.Angle(hit.normal, Vector3.up);
+                        if(slopeAngle <= 30)
+                        {
+                            Vector3 spawnPos = hit.point;
+                            Quaternion spawnRot = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                            Instantiate(firePrefabs[level], spawnPos, spawnRot);
+                        }
+                    }
                 }
             }
         }
