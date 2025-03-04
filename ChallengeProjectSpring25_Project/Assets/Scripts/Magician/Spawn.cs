03483bcadf3c397f1ca31bb8e1b63f802fa9f38a
Jake Ozer <jakobozer1@gmail.com>
2025-03-04 17:48:41 -0600

    debugstatement

--- ChallengeProjectSpring25_Project/Assets/Scripts/Magician/Spawn.cs
+++ ChallengeProjectSpring25_Project/Assets/Scripts/Magician/Spawn.cs
@@ -19,9 +19,11 @@ public class Spawn : MonoBehaviour
         //Vector3 offseft = new Vector3(Random.Range(-radius, radius), Random.Range(-radius,radius), Random.Range(-radius,radius));
         time += Time.deltaTime;//timer for spawning of the magicians
         Vector3 offset = Random.insideUnitSphere * radius;//found that calling insideUnitSphere is supposed to spawn a cirlce radius
+        
         Vector3 spawn = theBoss.transform.position + offset;
-        if (time >= 5f)
+        if (time >= 2f)
         {
+            //Debug.Log(Random.insideUnitSphere);
             Instantiate(theMagician, spawn, Quaternion.identity);
             time = 0;
         }
