f0a98e82d076738a6265c6144f0f646bfada8895
Jake Ozer <jakobozer1@gmail.com>
2025-03-22 15:49:29 -0500

    health pickup done and other thinsg

--- ChallengeProjectSpring25_Project/Assets/Scripts/Magician/Spawn.cs
+++ ChallengeProjectSpring25_Project/Assets/Scripts/Magician/Spawn.cs
@@ -26,9 +26,9 @@ public class Spawn : MonoBehaviour
             Vector3 offset = new Vector3(Random.insideUnitCircle.y * radius, .10f);//found that calling insideUnitSphere is supposed to spawn a cirlce radius
             offset.z = .5f;
             Vector3 spawn = theBoss.transform.position + offset;
-            Debug.Log("offset: " + offset);
-            Debug.Log("boss pos: " + theBoss.transform.position);
-            Debug.Log("spawn: " + spawn);
+            //Debug.Log("offset: " + offset);
+            //Debug.Log("boss pos: " + theBoss.transform.position);
+            //Debug.Log("spawn: " + spawn);
             Vector3 testOff = new Vector3(5, .1f, 5);
             //Debug.Log(Random.insideUnitSphere);
             var miniEnemy = Instantiate(theMagician, spawn, Quaternion.identity);
--- ChallengeProjectSpring25_Project/Assets/Scripts/PlayerFunctions/PlayerAttack.cs
+++ ChallengeProjectSpring25_Project/Assets/Scripts/PlayerFunctions/PlayerAttack.cs
@@ -17,7 +17,8 @@ public class PlayerAttack : MonoBehaviour
     {
         timer -= Time.deltaTime;
 
-        if(input.actions["Attack"].triggered && timer <= 0)
+        //hit enemy
+        if (input.actions["Attack"].triggered && timer <= 0)
         {
             timer = attackCooldown;
             if (animator.GetCurrentAnimatorStateInfo(0).IsName("swordattack"))
@@ -25,40 +26,27 @@ public class PlayerAttack : MonoBehaviour
                 animator.SetTrigger("reset");
             }
             animator.SetTrigger("attack");
-
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitinfo, attackRange, enemyLayer))
-            {
-                if (hitinfo.collider.gameObject.GetComponent<BossHealth>() != null || hitinfo.collider.gameObject.GetComponent<MiniEnemy>() != null)
-                {
-                    //Debug.Log("enemy hit");
-                    //hitinfo.collider.gameObject.GetComponent<BossHealth>().TakeDamage(playerDmg);
-                    attacking = true;
-                    currentAttackTarget = hitinfo.collider.gameObject;
-                }
-            }
-            else
-            {
-                attacking = false;
-            }
         }
 
-        Debug.DrawRay(transform.position, transform.forward * attackRange, Color.yellow);
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * attackRange, Color.yellow);
     }
 
     //used by animation event from sword anim to detect when player is at climax of swing
     public void PlayerAttackSignaled()
     {
-        if (attacking)
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward), out RaycastHit hitinfo, attackRange, enemyLayer))
         {
-            if(currentAttackTarget.GetComponent<BossHealth>() != null)
+            if (hitinfo.collider.gameObject.GetComponent<BossHealth>() != null || hitinfo.collider.gameObject.GetComponent<MiniEnemy>() != null)
             {
-                currentAttackTarget.GetComponent<BossHealth>().TakeDamage(playerDmg);
+                if (hitinfo.collider.gameObject.GetComponent<BossHealth>() != null)
+                {
+                    hitinfo.collider.gameObject.GetComponent<BossHealth>().TakeDamage(playerDmg);
+                }
+                else if (hitinfo.collider.gameObject.GetComponent<MiniEnemy>() != null)
+                {
+                    Destroy(hitinfo.collider.gameObject);
+                }
             }
-            if(currentAttackTarget.GetComponent<MiniEnemy>() != null)
-            {
-                Destroy(currentAttackTarget);
-            }
-            
         }
     }
 }
