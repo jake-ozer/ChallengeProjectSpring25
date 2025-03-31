a254062f5744f0b3d65cc53ce19c225fbe95859b
temka0 <dankwine007@gmail.com>
2025-03-30 19:02:55 -0500

    MeteorIndicator
    Created a script to detect whenever there is a meteor above the player. It also notifies the player via UI

---
+++ ChallengeProjectSpring25_Project/Assets/Scripts/UI/MeteorIndicator.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class MeteorIndicator : MonoBehaviour
+{
+
+    public GameObject meteorText;
+    public LayerMask meteorLayer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        meteorText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray meteorDetect = new Ray(transform.position + Vector3.up * 0.5f, Vector3.up); //0.5 is the offset
+        if(Physics.Raycast(meteorDetect, out hit, 100, meteorLayer))//100 is the height.
+        {
+            Debug.Log("test good!!!!");
+            meteorText.SetActive(true);
+        } else
+        {
+            meteorText.SetActive(false);
+        }
+    }
+}
