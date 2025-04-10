fae7660552198a087c1c43f2126cc27e5d6848b3
lennart3211 <82011287+lennart3211@users.noreply.github.com>
2025-04-09 21:54:42 -0500

    started implementing boss charge mechanic

--- ChallengeProjectSpring25_Project/Assets/Scripts/Boss/AI/DefaultBossAI.cs
+++ ChallengeProjectSpring25_Project/Assets/Scripts/Boss/AI/DefaultBossAI.cs
@@ -1,10 +1,22 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class DefaultBossAI : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     private NavMeshAgent agent;
+
+    [SerializeField] private float fastspeed = 20f;
+    [SerializeField] private float slowspeed = 0f;
+    [SerializeField] private float normalspeed = 5f;
+    [SerializeField] private float triggerDistance = 5f;
+    private float slowtimer;
+    private float fasttimer;
+    private float cooldown = 5f;
+    private bool isSlow;
+    private bool isFast;
 
     private void Start()
     {
@@ -14,9 +26,39 @@ public class DefaultBossAI : MonoBehaviour
 
     private void Update()
     {
-        if (agent != null && agent.enabled)
+        if (!agent || !agent.enabled) return;
+
+        agent.SetDestination(player.transform.position);
+
+        if (isSlow)
         {
-            agent.SetDestination(player.transform.position);
+            agent.transform.rotation = Quaternion.Slerp(Quaternion.LookRotation(player.transform.position - agent.transform.position), agent.transform.rotation, slowtimer/cooldown);
+
+            slowtimer -= Time.deltaTime;
+            if (slowtimer < 0f)
+            {
+                isSlow = false;
+                isFast = true;
+                fasttimer = cooldown;
+                agent.speed = fastspeed;
+            }
+        }
+
+        else if (isFast)
+        {
+            fasttimer -= Time.deltaTime;
+            if (fasttimer < 0f)
+            {
+                isFast = false;
+                agent.speed = normalspeed;
+            }
+        }
+        
+        else if (Vector3.Distance(player.transform.position, agent.transform.position) > triggerDistance)
+        {
+            isSlow = true;
+            slowtimer = cooldown;
+            agent.speed = slowspeed;
         }
     }
        
